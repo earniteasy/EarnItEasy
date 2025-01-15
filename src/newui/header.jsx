29fb51bedd0222c7e4dbc11f4c3d0d33b2803eb8
earniteasy <earniteasyapp@gmail.com>
2025-01-15 14:31:50 +0530

    Update header.jsx

--- src/newui/header.jsx
+++ src/newui/header.jsx
@@ -14,7 +14,7 @@ export default function NewHeader() {
       <NavbarContent justify="end">
         <p className="text-sm text-gray-600 font-regular w-28">For 18 years and above only</p>
         <NavbarItem className="hidden lg:flex bg-white border border-gray-300 rounded-md px-5 py-1.5 text-sm font-semibold">
-          <Link download href="../download/release-v3.0.apk">Download App</Link>
+          <Link download href="https://drive.google.com/uc?export=download&id=10l9Kbc8y74KzhbLwxtLhMJ13ijj6nLbS">Download App</Link>
         </NavbarItem>
         <NavbarItem className="hidden lg:flex bg-white border border-gray-300 rounded-md px-5 py-1.5 text-sm font-semibold">
           <Link>Contact</Link>
