d3fa58bc9b80c7ae202771c9fe2c15e4f7b63dc6
earniteasy <earniteasyapp@gmail.com>
2025-01-15 14:21:12 +0530

    Update header.jsx

--- src/newui/header.jsx
+++ src/newui/header.jsx
@@ -2,16 +2,6 @@ import React, { useState } from "react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link} from "@nextui-org/react";
 
 export default function NewHeader() {
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOpen = (event) => {
-    event.preventDefault();
-    setIsOpen(true);
-  };
-
-  const handleClose = () => setIsOpen(false);
-  
   return (
     <>
     <Navbar isBordered>
@@ -27,20 +17,10 @@ export default function NewHeader() {
           <Link download href="../download/release-v3.0.apk">Download App</Link>
         </NavbarItem>
         <NavbarItem className="hidden lg:flex bg-white border border-gray-300 rounded-md px-5 py-1.5 text-sm font-semibold">
-          <Link><a onClick={handleOpen}>Contact</a></Link>
+          <Link>Contact</Link>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
-    <ModalBox
-         isOpen={isAccountOpen}
-         onClose={handleAccountClose}
-         title="Contact Us"
-         bodyContent={
-          <div className="h-96">
-            <p>You can contact us by our email id: earniteasyapp@gmail.com</p>
-          </div>
-         }
-      />
     </>
   );
 }
